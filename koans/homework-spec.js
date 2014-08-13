8a5e23645ad35ba726210d9d679941f09a4f198b
ddark <log9099@gmail.com>
2014-08-13 21:41:30 +0400

    Added loftblog homework

---
+++ koans/homework-spec.js
@@ -0,0 +1,25 @@
+/**
+ * Created by XX on 13.08.2014.
+ */
+describe("Решаем загадки сложения объектов",function(){
+    it("Должно помоч понять магию JS", function(){
+        expect([]+[]).toBe(FILL_ME_IN)
+    });
+
+    it("Должно помоч понять магию JS", function(){
+        expect([]+{}).toBe(FILL_ME_IN)
+    });
+
+    it("Должно помоч понять магию JS", function(){
+        expect([]+{}).toBe(FILL_ME_IN)
+    });
+
+    it("Должно помоч понять магию JS", function(){
+        expect({}+{}).toBe(FILL_ME_IN)
+    });
+
+    it("Должно помоч понять магию JS", function(){
+        expect({}+[]).toBe(FILL_ME_IN)
+    });
+
+})
